3e666bf217b038abbc6e45ec297af86b8c4eaa0c
maintn24 <maintn124@gmail.com>
2024-10-10 22:08:24 +0700

    fix-backend: confgi cors and add cookie

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -1,6 +1,20 @@
 import * as mongoose from "mongoose";
-
+import express from "express";
+import cors from "cors";
 const MONGODB_URI = process.env.MONGODB_URI;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const BACKEND_URL = process.env.BACKEND_URL;
+
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+    origin: FRONTEND_URL || BACKEND_URL, // Allow requests from frontend
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true // Allow cookies to be sent
+};
+app.use(cors(corsOptions));
 
 const connect = async () => {
     const connectionState = mongoose.connection.readyState;
@@ -20,7 +34,10 @@ const connect = async () => {
             dbName: 'SystemAd',
             bufferCommands: true
         })
-        console.log("Connected");
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("server running on http://localhost:3000");
+        });
     } catch (e) {
         console.log("Error: " + e);
         throw new Error("Error: " + e);
@@ -28,3 +45,4 @@ const connect = async () => {
 }
 
 export default connect;
+
