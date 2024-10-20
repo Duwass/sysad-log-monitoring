edb70a493093dfcd95de6ee80e37bd39c4ba55f7
maintn24 <maintn124@gmail.com>
2024-10-21 00:10:18 +0700

    refactor directory

--- express-backend/src/routes/users.route.ts
+++ express-backend/src/routes/users.route.ts
@@ -3,7 +3,7 @@ import {userRegister, userLogin} from "../controllers/users.controller";
 
 const router = express.Router();
 
-router.post('/user/register', userRegister);
-router.post('/user/login', userLogin);
+router.post('/register', userRegister);
+router.post('/login', userLogin);
 
 export default router;
